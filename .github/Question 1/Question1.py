8f1eadd43d0f875b3cdc7ff4658a498fc6c1f6e5
achille2018 <a.tanwouo@alustudent.com>
2021-07-29 17:24:56 +0200

    Question1

---
+++ .github/Question 1/Question1.py
@@ -0,0 +1,13 @@
+
+
+
+a= (0,1, 2, 3, 4, 5, 6, 7, 8, 9,12,16)
+odd = 0
+even = 0
+for i in a:
+        if  i % 2==0:
+    	      even+=1
+        else:
+    	     odd+=1
+print("Number of even numbers :",even)
+print("Number of odd numbers :",odd)
